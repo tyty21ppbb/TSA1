74e207f57e52ba54d0b0bcedebb205a30186036d
tyty21ppbb <panulde.artlaurenze21@gmail.com>
2026-09-26 00:40:04 +0800

    Update Database.php to use environment variables securely

--- TSA1/myproject/app/Config/Database.php
+++ TSA1/myproject/app/Config/Database.php
@@ -4,96 +4,87 @@ namespace Config;
 
 use CodeIgniter\Database\Config;
 
-/**
- * Database Configuration
- */
 class Database extends Config
 {
+    /**
+     * The directory that holds the Migrations
+     * and Seeds directories.
+     */
     public string $filesPath = APPPATH . 'Database' . DIRECTORY_SEPARATOR;
+
+    /**
+     * Lets you choose which connection group to use if no
+     * other one is specified.
+     */
     public string $defaultGroup = 'default';
 
-    public array $default = [
-        'DSN'          => '',
-        'hostname'     => '127.0.0.1',
-        'username'     => 'root',
-        'password'     => '',
-        'database'     => '',
-        'DBDriver'     => 'MySQLi',
-        'DBPrefix'     => '',
-        'pConnect'     => false,
-        'DBDebug'      => true,
-        'charset'      => 'utf8mb4',
-        'DBCollat'     => 'utf8mb4_general_ci',
-        'swapPre'      => '',
-        'encrypt'      => [
-            'ssl_verify' => false,
-        ],
-        'compress'     => false,
-        'strictOn'     => false,
-        'failover'     => [],
-        'port'         => 3306,
-        'numberNative' => false,
-        'dateFormat'   => [
-            'date'     => 'Y-m-d',
-            'datetime' => 'Y-m-d H:i:s',
-            'time'     => 'H:i:s',
-        ],
+    /**
+     * The default database connection.
+     */
+    public $default = [
+        'DSN'      => '',
+        'hostname' => '', 
+        'username' => 'avnadmin',                  
+        'password' => '', // Leave blank; it will be injected safely via Render Environment Variables
+        'database' => 'defaultdb',                 
+        'DBDriver' => 'MySQLi',
+        'DBPrefix' => '',
+        'pConnect' => false,
+        'DBDebug'  => (ENVIRONMENT !== 'development'),
+        'charset'  => 'utf8mb4',
+        'DBCollat' => 'utf8mb4_general_ci',
+        'swapPre'  => '',
+        'encrypt'  => true,
+        'compress' => false,
+        'strictOn' => false,
+        'failover' => [],
+        'port'     => 10441,                     
     ];
 
+    /**
+     * This database connection is used when running PHPUnit database tests.
+     */
     public array $tests = [
-        'DSN'          => '',
-        'hostname'     => '127.0.0.1',
-        'username'     => '',
-        'password'     => '',
-        'database'     => ':memory:',
-        'DBDriver'     => 'SQLite3',
-        'DBPrefix'     => 'db_',
-        'pConnect'     => false,
-        'DBDebug'      => true,
-        'charset'      => 'utf8',
-        'DBCollat'     => '',
-        'swapPre'      => '',
-        'encrypt'      => false,
-        'compress'     => false,
-        'strictOn'     => true,
-        'failover'     => [],
-        'port'         => 3306,
+        'DSN'         => '',
+        'hostname'    => '127.0.0.1',
+        'username'    => '',
+        'password'    => '',
+        'database'    => ':memory:',
+        'DBDriver'    => 'SQLite3',
+        'DBPrefix'    => '',
+        'pConnect'    => false,
+        'DBDebug'     => true,
+        'charset'     => 'utf8',
+        'DBCollat'    => '',
+        'swapPre'     => '',
+        'encrypt'     => false,
+        'compress'    => false,
+        'strictOn'    => false,
+        'failover'    => [],
+        'port'        => 3306,
         'foreignKeys' => true,
         'busyTimeout' => 1000,
-        'synchronous' => null,
-        'dateFormat'   => [
-            'date'     => 'Y-m-d',
-            'datetime' => 'Y-m-d H:i:s',
-            'time'     => 'H:i:s',
-        ],
     ];
 
     public function __construct()
     {
         parent::__construct();
 
-        // Direct lookup from Docker/Render environment variables
-        if ($host = getenv('database.default.hostname')) {
-            $this->default['hostname'] = $host;
+        // Override settings dynamically from Render Environment Variables
+        if (getenv('database.default.hostname')) {
+            $this->default['hostname'] = getenv('database.default.hostname');
         }
-        if ($user = getenv('database.default.username')) {
-            $this->default['username'] = $user;
+        if (getenv('database.default.database')) {
+            $this->default['database'] = getenv('database.default.database');
         }
-        if ($pass = getenv('database.default.password')) {
-            $this->default['password'] = $pass;
+        if (getenv('database.default.username')) {
+            $this->default['username'] = getenv('database.default.username');
         }
-        if ($db = getenv('database.default.database')) {
-            $this->default['database'] = $db;
+        if (getenv('database.default.password')) {
+            $this->default['password'] = getenv('database.default.password');
         }
-        if ($port = getenv('database.default.port')) {
-            $this->default['port'] = (int) $port;
-        }
-        if ($driver = getenv('database.default.DBDriver')) {
-            $this->default['DBDriver'] = $driver;
-        }
-
-        if (ENVIRONMENT === 'testing') {
-            $this->defaultGroup = 'tests';
+        if (getenv('database.default.port')) {
+            $this->default['port'] = (int) getenv('database.default.port');
         }
     }
 }
